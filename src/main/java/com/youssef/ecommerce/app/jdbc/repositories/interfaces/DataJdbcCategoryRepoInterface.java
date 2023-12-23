df1e760eb084794401cd338a240341da97428a3d
Youssef Gamal <youssef.gamal@intercom.com.eg>
2023-12-23 14:24:06 +0200

    performance/Optimizing Adding new Product to db, when we add new product we check that the assigned Categories are exist or not so we modified the retrieval of the Category and minimizing un-necessary sql queries

--- src/main/java/com/youssef/ecommerce/app/jdbc/repositories/interfaces/DataJdbcCategoryRepoInterface.java
+++ src/main/java/com/youssef/ecommerce/app/jdbc/repositories/interfaces/DataJdbcCategoryRepoInterface.java
@@ -1,17 +1,22 @@
 package com.youssef.ecommerce.app.jdbc.repositories.interfaces;
 
 import com.youssef.ecommerce.app.jdbc.entities.Category;
+import com.youssef.ecommerce.app.jdbc.mappers.sqls_rows.CategoryRowMapper;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
-public interface DataJdbcCategoryRepoInterface extends CrudRepository<Category,Integer> {
+public interface DataJdbcCategoryRepoInterface extends CrudRepository<Category, Integer> {
 
 
     boolean existsByNameIgnoreCase(String categoryName);
 
 
+    @Query(value = "SELECT * FROM categories WHERE category_id = :c_id LIMIT 1" , rowMapperClass =  CategoryRowMapper.class)
+    Optional<Category> findById(@Param("c_id") Integer c_id);
 }
