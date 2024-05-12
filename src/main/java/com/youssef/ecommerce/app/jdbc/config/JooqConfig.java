4005f622083e86bd5275a20692ac99e7f7947f38
Youssef Gamal <youssef.gamal@intercom.com.eg>
2024-05-12 14:57:41 +0300

    feat: Added Java Config for Logging SQLs with JOOQ

--- src/main/java/com/youssef/ecommerce/app/jdbc/config/JooqConfig.java
+++ src/main/java/com/youssef/ecommerce/app/jdbc/config/JooqConfig.java
@@ -2,6 +2,7 @@ package com.youssef.ecommerce.app.jdbc.config;
 
 import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
+import org.jooq.conf.Settings;
 import org.jooq.impl.DataSourceConnectionProvider;
 import org.jooq.impl.DefaultConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -18,6 +19,10 @@ public class JooqConfig {
         DefaultConfiguration configuration = new DefaultConfiguration();
         configuration.set(connectionProvider);
         configuration.set(SQLDialect.MYSQL); // Set your SQLDialect here
+        Settings settings = new Settings();
+        settings.setRenderFormatted(true);
+        settings.setExecuteLogging(true);
+        configuration.setSettings(settings);
         return configuration.dsl();
     }
 }
